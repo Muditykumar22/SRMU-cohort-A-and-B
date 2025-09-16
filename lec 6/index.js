4d05f742a40d827594f3878215ba7968a0d63efe
Mudity kumar <mudity.kumar_cs22@gla.ac.in>
2025-09-16 20:00:08 +0530

    JS started

---
+++ lec 6/index.js
@@ -0,0 +1,46 @@
+// let x=70;
+// {
+//     let x=50;
+// const y=10;
+// console.log(x);
+// x= 20;//temporal deadzone or cannot access.
+// }
+// console.log(x);
+// // {
+// // console.log("abhay");
+// // {
+// //     console.log(x*x);
+// //     let y=40;
+// //     console.log(y);
+
+// // }
+// // console.log(y);
+// // }
+
+// let x=89;
+// {
+//     console.log(x);
+//     x=90;
+// }
+// console.log(x)
+// x=91;
+// console.log(x);
+// console.log(typeof(x));
+// let k = "undefined";
+// console.log(k);
+// console.log(typeof(k));
+// let arr= [2,3,4,5,6,7,"hero",true, null, undefined,1,2,6,8,4];
+// console.log(arr);
+// for (let i =0; i<arr.length;i++){
+//     console.log(typeof(arr[i]));
+// }
+let x={
+    name:"ankush",
+    age:2,
+    address:"dholakpur",
+    name:"Aniket"
+}
+console.log(x);
+console.log(x. address ,  x.name)
+
+console.log(`Name: ${x.name}, Address: ${x.address}`);
